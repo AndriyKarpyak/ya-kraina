1fa3aeb3c1db6c4e39224d4b75f5c1f01e6b7441
Andriy Karpyak <seraronax@gmail.com>
2021-09-14 15:55:12 +0300

    Працюю над фінальним екраном сценарію

--- ya_kraina/src/com/karp/yakraina/client/views/StoryPointsView.java
+++ ya_kraina/src/com/karp/yakraina/client/views/StoryPointsView.java
@@ -1,7 +1,13 @@
 package com.karp.yakraina.client.views;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlUtils;
 import com.google.gwt.safehtml.shared.UriUtils;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -9,14 +15,16 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Widget;
 import com.karp.yakraina.client.events.ColorThemeChangeEvent;
-import com.karp.yakraina.client.events.ShowNextViewEvent;
+import com.karp.yakraina.client.events.StoryEndEvent;
 import com.karp.yakraina.client.model.session.GameSession;
 import com.karp.yakraina.client.model.story.StoryJs;
 import com.karp.yakraina.client.model.story.StorySubResultJs;
+import com.karp.yakraina.client.model.story.SummaryConditionJs;
 import com.karp.yakraina.client.widgets.MatteBytton;
 
 public class StoryPointsView extends Composite {
@@ -27,33 +35,71 @@ public class StoryPointsView extends Composite {
 	}
 
 	@UiField
-	InlineLabel resultText;
+	HTMLPanel pointsPanel;
+	
+	@UiField
+	InlineLabel pointsText;
 	
 	@UiField
 	HTML storyNameLabel;
 	
 	@UiField
+	HTML storySummaryText;
+	
+	@UiField
 	Image storyIcon;
 
 	@UiField
+	HTMLPanel singleSummaryPanel;
+	
+	@UiField
+	Image emoji;
+	
+	@UiField
+	HTML singleSummaryText;
+	
+	@UiField
+	MatteBytton button_Next;
+	
+	@UiField
 	MatteBytton button_Accept;
+	
+	
+	private List<SafeHtml> messages = new ArrayList<>();
+
+	private Iterator<SafeHtml> messagesIterator;
 
 	public StoryPointsView() {
 		initWidget(uiBinder.createAndBindUi(this));
 		
+		pointsPanel.getElement().getStyle().setDisplay(Display.NONE);
+		singleSummaryPanel.getElement().getStyle().setDisplay(Display.NONE);
+		
+		button_Next.setHTML(SafeHtmlUtils.fromTrustedString("ДАЛІ &#x2192;"));
 		button_Accept.setHTML(SafeHtmlUtils.fromTrustedString("ДАЛІ &#x2192;"));
 	}
 
 	@UiHandler("button_Accept")
 	public void onAcceptPlayerName(ClickEvent event) {
-		ShowNextViewEvent.fire(new StorySummariesView());
+		StoryEndEvent.fire();
+//		ShowNextViewEvent.fire(new StorySummariesView());
+	}
+	
+	@UiHandler("button_Next")
+	public void onNextSummaryMessage(ClickEvent event) {
+		if (messagesIterator.hasNext())
+			nextMessage(messagesIterator.next());
+		else {
+			singleSummaryPanel.getElement().getStyle().setDisplay(Display.NONE);
+			pointsPanel.getElement().getStyle().clearDisplay();
+		}
 	}
 
 	@Override
 	protected void onLoad() {
-
+		
 		ColorThemeChangeEvent.fire("themeDark");
-
+		
 		StoryJs story = GameSession.get().getActiveStory();
 
 		storyNameLabel.setText(story.getName());
@@ -63,12 +109,41 @@ public class StoryPointsView extends Composite {
 
 		for (StorySubResultJs finalState : GameSession.get().getActiveStoryResults()) {
 
+			if (finalState.getText() != null && !finalState.getText().isEmpty()) {
+				messages.add(SafeHtmlUtils.fromTrustedString(finalState.getText()));
+			}
+			
 			points += finalState.getPoints();
 		}
 
-		resultText.setText(String.valueOf(points));
+		pointsText.setText(String.valueOf(points));
+		
+		List<SummaryConditionJs> conditions = GameSession.get().getActiveStorySummaryConditions();
+
+		for (int i = conditions.size() - 1; i >= 0; i--) {
+			SummaryConditionJs condition = conditions.get(i);
+
+			if (points > condition.getLowerBound() || (points == 0 && points == condition.getLowerBound())) {
+				storySummaryText.setHTML(SafeHtmlUtils.fromTrustedString(condition.getText()));
+				break;
+			}
+
+		}
 
 		super.onLoad();
+		
+		messagesIterator = messages.iterator();
+		
+		if (messagesIterator.hasNext()) 
+			nextMessage(messagesIterator.next());
+		else
+			pointsPanel.getElement().getStyle().clearDisplay();
+	}
+	
+	private void nextMessage(SafeHtml message) {
+		emoji.setUrl(UriUtils.fromTrustedString("images/emoji/smiley.svg"));
+		singleSummaryPanel.getElement().getStyle().clearDisplay();
+		singleSummaryText.setHTML(message);
 	}
 
 }
